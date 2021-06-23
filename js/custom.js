270ae3c057a8d422550ec1775a47718abff40e11
Narayan Chandra Das <narayandas5606@gmail.com>
2021-06-23 09:18:51 -0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	 'items':3,
+  	 'margin':20,
+  	 'loop' : true,
+  	 'nav' : true,
+  	 'dots': true,
+  	 'mouseDrag': true,
+  	 'autoplay': true,
+  	 'autoplayTimeout': 3000,
+  	 'autoplayHoverPause': true,
+  	 'pullDrag': false,
+  	 'smartSpeed': 2000,
+  	 'fluidSpeed': 5000,
+  	 // 'autoplaySpeed': true,
+  });
+ 
+});
